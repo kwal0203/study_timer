a963a7c72006228a26445246dab129caff929e9d
Kane Walter <kwal0203@gmail.com>
2017-07-18 21:10:05 +1000

    Completed daily countdown timer functionality

--- timer.py
+++ timer.py
@@ -1,7 +1,7 @@
 #Todo:
 #1. Add todo list
 #2. Manually enter minutes DONE
-#3. Daily minute countdown
+#3. Daily minute countdown DONE
 #4. Add in SQR3 memory function
 
 import os
@@ -10,15 +10,43 @@ import sys
 import sqlite3
 from math import floor
 
-def timer():
+def timer(subject):
     try:
-        start = int(time.time())
+        #get daily time remaining for subject
+        db = sqlite3.connect('User.db') 
+        cursor = db.cursor()
+        sql = "SELECT " + subject + " FROM users WHERE ID = 1";
+        cursor.execute(sql)
+        minsLeft = cursor.fetchone()[0]
+        db.close()
+ 
+        start = time.time()
         while True:
             os.system('clear')
-            print int(time.time()) - start, "seconds elapsed"
+            #current study length
+            elapsed = int(time.time() - start)
+            print elapsed, "seconds elapsed\n"
+            if elapsed > 1500:
+                print "Take a break\n"
+            #decrement daily subject timer
+            minsLeft -= 1
+            print int(minsLeft), "seconds left"
+
             time.sleep(1)
     except KeyboardInterrupt:
-       return time.time() - start 
+        return_time = int(time.time() - start)
+    
+        #get daily time remaining for subject
+        db = sqlite3.connect('User.db') 
+        cursor = db.cursor()
+        
+        #sql command to decrement daily timer
+        sql = 'UPDATE users SET ' + subject + '=' + subject + ' - ' + \
+        str(return_time) + ' WHERE ID = 1'
+        cursor.execute(sql)
+        db.commit()
+        db.close()
+        return return_time 
 
 def rest():
     time.sleep(1)
@@ -29,7 +57,8 @@ def main_menu():
     print "2.) Add time manually"
     print "3.) Run statistics"
     print "4.) See spaced repetition schedule"
-    print "5.) Exit"
+    print "5.) Reset daily time counters"
+    print "6.) Exit"
     print "\n> ",
 
 def srs():
@@ -48,20 +77,17 @@ def read_database():
     try:
         cursor.execute(sql)
         results = cursor.fetchone()
-        name = results[1]
-        subj1 = results[2]
-        subj2 = results[3]
-        subj3 = results[4]
-        subj4 = results[5]
-        subj5 = results[6]
-        subj6 = results[7]
-        print "\nName: %s" % name
-        print "COMP9444: %d" % subj1
-        print "COMP3222: %d" % subj2
-        print "COMP2041: %d" % subj3
-        print "MATH3411: %d" % subj4
-        print "Project:  %d" % subj5
-        print "Break:    %d" % subj6
+        print "\nName: %s"    % results[1]
+        print "COMP9444:  %d" % results[2]
+        print "COMP3222:  %d" % results[3]
+        print "COMP2041:  %d" % results[4]
+        print "MATH3411:  %d" % results[5]
+        print "Project:   %d" % results[6]
+        print "Break:     %d" % results[7]
+        print "COMP9444T: %d" % results[8]
+        print "COMP3222T: %d" % results[9]
+        print "COMP2041T: %d" % results[10]
+        print "MATH3411T: %d" % results[11]
         print "\nPress any key to continue",
         finish = raw_input()
     except:
@@ -87,16 +113,33 @@ def add_time(subject, time):
     sql = 'UPDATE users SET ' + subject + '=' + subject + ' + ' + \
         str(time) + ' WHERE ID = 1'
     
-    #debug
-    #print "SQL: %s" % sql
-    #deb = raw_input()
-    
     #carry out database operation
     db = sqlite3.connect('User.db') 
     cursor = db.cursor()
     cursor.execute(sql)
     db.commit()
     db.close()
+
+def reset():
+    db = sqlite3.connect('User.db') 
+    sql = "UPDATE users SET COMP9444_TIME = 7200"
+    cursor = db.cursor()
+    cursor.execute(sql)
+    db.commit()
+    sql = "UPDATE users SET COMP3222_TIME = 7200"
+    cursor = db.cursor()
+    cursor.execute(sql)
+    db.commit()
+    sql = "UPDATE users SET COMP2041_TIME = 7200"
+    cursor = db.cursor()
+    cursor.execute(sql)
+    db.commit()
+    sql = "UPDATE users SET MATH3411_TIME = 7200"
+    cursor = db.cursor()
+    cursor.execute(sql)
+    db.commit()
+    db.close()
+    
 
 def subject_menu():
     print "\nWhich subject?"
@@ -115,29 +158,41 @@ def clock():
 
         if command == '1':
             #raw time in seconds
-            time = timer()
-            time = int(time)
+            #time = timer()
+            #time = int(time)
 
             #select subject to add time to
             subject_menu()
             subject = raw_input();
             
             if subject == '1':
+                time = timer("COMP9444_TIME")
+                time = int(time)
                 add_time("COMP9444", time)
                 rest()
             elif subject == '2':
+                time = timer("COMP3222_TIME")
+                time = int(time)
                 add_time("COMP3222", time)
                 rest()
             elif subject == '3':
+                time = timer("COMP2041_TIME")
+                time = int(time)
                 add_time("COMP2041", time)
                 rest()
             elif subject == '4':
+                time = timer("MATH3411_TIME")
+                time = int(time)
                 add_time("MATH3411", time)
                 rest()
             elif subject == '5':
+                time = timer("PROJECT_TIME")
+                time = int(time)
                 add_time("PROJECT", time)
                 rest()
             elif subject == '6':
+                time = timer("BREAK_TIME")
+                time = int(time)
                 add_time("BREAK", time)
                 rest()
         elif command == '2':
@@ -171,6 +226,11 @@ def clock():
         elif command == '3':
             read_database()
         elif command == '4':
+            srs()
+            raw_input("Press any key to continue")
+        elif command == '5':
+            reset()
+        elif command == '6':
             print "Goodbye"
             exit(0)
 clock()
